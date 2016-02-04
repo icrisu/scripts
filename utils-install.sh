59b112bdefa76ecd71c08d1ec545bb6d7270b499
Ionel Crisu <crisu.ionel@gmail.com>
2016-02-04 09:17:56 -0500

    add utils script (grunt-cli, nodemon)

---
+++ utils-install.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+#Only use after install node & npm
+echo 'update npm....'
+npm update -g npm
+
+echo 'install nodemnon'
+npm install -g nodemon
+
+echo 'install grunt cli'
+npm install -g grunt-cli
