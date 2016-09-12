ec90ecde9014cbadba30a22b66b616c0c4dd339e
Ionel Crisu <crisu.ionel@gmail.com>
2016-09-12 14:45:28 +0300

    add install support for express, nodemon, grunt

--- node-download-compile-install.sh
+++ node-download-compile-install.sh
@@ -6,3 +6,11 @@ apt-get install curl
 curl -sL https://deb.nodesource.com/setup_4.x -o nodesource_setup.sh
 bash nodesource_setup.sh
 apt-get install nodejs
+echo node -v
+echo npm -v
+echo "install express generator ..."
+npm install express-generator -g
+echo "install nodemon ..."
+npm install -g nodemon
+echo "install grunt cli ..."
+npm install -g grunt-cli
